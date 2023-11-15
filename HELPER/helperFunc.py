cfa1b2396ce692d1b144636d4951ba3821468cf1
Shiro <miyamuraizumi3dsp@gmail.com>
2023-11-15 15:03:43 +0530

    Update helperFunc.py

--- HELPER/helperFunc.py
+++ HELPER/helperFunc.py
@@ -24,3 +24,27 @@ async def handle_exception(Bot):
 def system_reboot(UID): 
     import os
     os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for i in range(len(time_list)):
+        time_list[i] = str(time_list[i]) + time_suffix_list[i]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
